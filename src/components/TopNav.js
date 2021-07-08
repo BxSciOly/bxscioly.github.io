a15134084a8bb441684b9cb65caad969f943b7e9
bananaman441 <thesoupmanguy@gmail.com>
2021-07-08 14:08:04 -0400

    topnav

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -6,9 +6,16 @@ import styled from "styled-components";
 const TopNav = () => {
   return (
     <Navbar>
-      <FiIcons.FiMenu />
-      <SiIcons.SiAtom style={{ marginLeft: "40px" }} />
-      <h1 style={{ fontSize: "30px", marginLeft: "10px" }}>BxScioly</h1>
+        <LeftItems>
+            <FiIcons.FiMenu />
+            <SiIcons.SiAtom style={{ marginLeft: "40px" }} />
+            <h1 style={{ fontSize: "25px", marginLeft: "10px", fontWeight: "100" }}>
+                Bronx Science Olympiad
+            </h1>
+      </LeftItems>
+      <RightItems>
+          <Button>Join Us!</Button>
+      </RightItems>
     </Navbar>
   );
 };
@@ -16,12 +23,37 @@ const TopNav = () => {
 const Navbar = styled.div`
   width: 100%;
   border-bottom: 2px solid black;
-  height: 80px;
+  height: 70px;
   display: flex;
   align-items: center;
-  font-size: 40px;
+  font-size: 30px;
   padding: 0 20px 0 20px;
-
+  justify-content: space-between;
 `;
 
+const LeftItems = styled.div`
+    width: 480px;
+    height: 70px;
+    display: flex;
+    align-items: center;
+`;
+const RightItems = styled.div`
+    width: 100px;
+    height: 70px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 20px;
+`;
+
+const Button = styled.button`
+    border: 1px solid black;  
+    width: 70px;
+    height: 30px;
+    background: white;
+    
+    &:hover {
+        cursor: pointer;
+    }
+`
 export default TopNav;
