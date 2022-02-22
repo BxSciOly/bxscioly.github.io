4e13ba1484209334cae2c6e56acd7d044b7603e8
warren yun <wyun2006@gmail.com>
2022-02-22 01:30:12 -0500

    Clean up board component

--- src/pages/otherComponents/Board/Board.js
+++ src/pages/otherComponents/Board/Board.js
@@ -1,42 +1,67 @@
 import React from "react";
 import styled from "styled-components";
 import BoardData from "./BoardData";
-import "./Board.css";
 
 const Board = () => {
-    return (
-        <div className="container">
-            {BoardData.map((member, i) => (
-                <Member>
-                    <div>
-                        <img src={member.background} alt={member.name} className="pfp"/>
-                    </div>
-                    <Name>{member.name}</Name>
-                    <Pos>{member.title}</Pos>
-                </Member>
-            ))}
-        </div>
-    );
-}
+  return (
+    <>
+      <Title>BOARD</Title>
+      <Container>
+        {BoardData.map((member, i) => (
+          <Member>
+            <div>
+              <Image src={member.background} alt={member.name} />
+            </div>
+            <Name>{member.name}</Name>
+            <Pos>{member.title}</Pos>
+          </Member>
+        ))}
+      </Container>
+    </>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  padding: 3rem;
+`;
+
+const Image = styled.img`
+  text-align: center;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+`;
+
+const Title = styled.h1`
+  color: #fff;
+  font-size: 350%;
+  padding: 2rem;
+  border-bottom: 2px solid var(--primary-color);
+`;
 
 const Member = styled.div`
-    padding: 1rem;
-    text-align: center;
-    display: grid;
-    grid-template-areas:
-        "pfp"
-        "name"
-        "desc";
-    transition: 200ms ease;
-    margin-right: 4rem;
+  padding: 1rem;
+  text-align: center;
+  display: grid;
+  grid-template-areas:
+    "pfp"
+    "name"
+    "desc";
+  transition: 200ms ease;
+  margin: 0 2rem;
 `;
 
 const Name = styled.p`
-    font-size: 150%;
+  font-size: 150%;
 `;
 
 const Pos = styled.p`
-    font-size: 100%;
+  font-size: 100%;
 `;
 
 export default Board;
