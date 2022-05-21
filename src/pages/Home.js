f4fc9cfe03b6d1adbefc32f203c730fe5b3499dc
Aaron <68828516+aaronbiscotti@users.noreply.github.com>
2022-05-21 10:05:52 -0400

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,67 +3,88 @@ import { useMediaQuery } from "@react-hook/media-query";
 import styled from "styled-components";
 import "./style/Homestyle.css";
 import Upcoming from "./homeComponents/Upcoming/Upcoming";
-import Timeline from "./homeComponents/Timeline/Timeline";
 import Info from "./Info/Info";
-import Background from "./background.mp4";
+import arrow from "./arrow.svg";
+import TextTransition from "react-text-transition";
+import background from "./background.mp4";
 
+const TEXTS = [
+  "Exploring the world of science",
+  "Scioly to the moon🚀🌑🌑",
+  "Competing since 2017",
+  "4x States qualifiers",
+];
 
 const Home = () => {
-  const [offsetY, setOffsetY] = useState(0);
-  const handleScroll = () => setOffsetY(window.pageYOffset);
-
+  const [index, setIndex] = useState(0);
   const isPageWide = useMediaQuery("(max-width: 600px)");
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
+    const intervalId = setInterval(() => setIndex((index) => index + 1), 3000);
+    return () => clearTimeout(intervalId);
   }, []);
 
   return (
-    <div className="page">
-      {isPageWide ? null : (
+    <>
+      <div className="page">
         <video autoPlay loop muted className="background">
-          <source src={Background} type="video/mp4" />
+          <source src={background} type="video/mp4" />
         </video>
-      )}
-      <div className="home">
-        <BackgroundDiv
-          style={{
-            transform: `translateY(${offsetY * 0.1}px)`,
-            transition: "0s all",
-          }}
-        >
-          <div className="bxsci">BRONX SCIENCE</div>
-          <div className="scioly">
-            <Title>SCIENCE OLYMPIAD</Title>
-          </div>
+        <BackgroundDiv>
+          <MainTitle>
+            The Bronx High School of{" "}
+            <span style={{ color: "#f2d30a" }}>Science</span>
+            <br />
+            <span style={{ color: "#f2d30a" }}>Science</span> Olympiad
+          </MainTitle>
 
-          <div className="desc">EXPLORING THE WORLD</div>
-          <div className="desc 2">OF SCIENCE</div>
-          <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLSdidklVH3t7vzymGo1uSvSvDrlLnfN4UMOyyeRGIg5WgVvtKw/viewform"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <JoinButton>JOIN US</JoinButton>
-          </a>
+          <TextTransition className="desc" text={TEXTS[index % TEXTS.length]}>
+            Exploring the world of science
+          </TextTransition>
+          <p className="wrap">
+            <a
+              href="https://docs.google.com/forms/d/e/1FAIpQLSdidklVH3t7vzymGo1uSvSvDrlLnfN4UMOyyeRGIg5WgVvtKw/viewform"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                margin: "0",
+                display: "flex",
+                flexBasis: "auto",
+              }}
+            >
+              <JoinButton>
+                <p className="join">JOIN US</p>
+                <img
+                  src={arrow}
+                  style={{ height: "15px", marginLeft: "15px" }}
+                />
+              </JoinButton>
+            </a>
+          </p>
         </BackgroundDiv>
       </div>
       <HomeBackground>
         <Info />
         <Upcoming />
       </HomeBackground>
-    </div>
+    </>
   );
 };
 
+const MainTitle = styled.h2`
+  z-index: 100;
+  font-size: 4rem;
+  color: white;
+  font-weight: 700;
+  margin-bottom: 10px;
+`;
+
 const BackgroundDiv = styled.div`
-  width: 100%;
+  z-index: 100;
   height: 700px;
   display: flex;
   justify-content: center;
-  align-items: center;
+
   flex-direction: column;
   margin-top: -50px;
 
@@ -73,12 +94,15 @@ const BackgroundDiv = styled.div`
 `;
 
 const HomeBackground = styled.div`
-  width: 80%;
+  z-index: 800;
   display: flex;
   flex-wrap: flex;
+  width: 100%;
+  background: #f9fafb;
+  padding-top: 150px;
+  padding-bottom: 100px;
   flex-direction: column;
   align-items: center;
-  background: none;
 
   @media (max-width: 600px) {
     width: 100%;
@@ -87,18 +111,18 @@ const HomeBackground = styled.div`
 
 const JoinButton = styled.button`
   margin-top: 40px;
-  width: 300px;
-  height: 60px;
-  font-size: 20px;
+  background: none;
+  display: flex;
+  align-items: center;
+  font-size: 18px;
   border: none;
+  color: var(--secondary-color);
   font-weight: 600;
-  border-radius: 30px;
   cursor: pointer;
   transition: 200ms ease;
 
   &:hover {
-    background-color: var(--primary-color);
-    box-shadow: 0 0 10px var(--primary-color);
+    transform: translate(5px, 0);
   }
 
   @media (max-width: 600px) {
@@ -107,15 +131,4 @@ const JoinButton = styled.button`
     margin-bottom: 8rem;
   }
 `;
-
-const Title = styled.h3`
-  color: var(--primary-color);
-  text-shadow: 0 0 12px var(--primary-color);
-
-  @media (max-width: 600px) {
-    margin-top: 2rem;
-    text-align: center;
-  }
-`;
-
 export default Home;
