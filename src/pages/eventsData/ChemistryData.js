f94373d1f88bbef05dc5bd4d969f38582b37038b
Aaron <68828516+aaronbiscotti@users.noreply.github.com>
2022-05-21 10:13:49 -0400

    Update ChemistryData.js

--- src/pages/eventsData/ChemistryData.js
+++ src/pages/eventsData/ChemistryData.js
@@ -1,21 +1,24 @@
 const ChemistryData = [
   {
     eventName: "Chem Lab",
+    path: "chemistry-lab",
     description:
       "Chemistry Lab is an event where participants must learn the year's selected aspects of chemistry (Aqueous Solutions, Redox Reactions for 2021-2022) and perform a lab or a set of labs regarding those topics.",
-    link: "https://www.soinc.org/chem-lab-c"
+    link: "https://www.soinc.org/chem-lab-c",
   },
   {
     eventName: "Environmental Chemistry",
+    path: "environmental-chemistry",
     description:
       "Environmental Chemistry tests students on their knowledge of environmental chemistry, including chemical reactions, characterization, and quantification of soil properties and nutrients. ",
-    link: "https://www.soinc.org/environmental-chemistry-c"
+    link: "https://www.soinc.org/environmental-chemistry-c",
   },
   {
     eventName: "Forensics",
+    path: "forensics",
     description:
       "Forensics involves the identification of powders, polymers, fibers, and hair samples, blood serum and fingerprint analysis, and interpretation of chromatography. Given a scenario and some possible suspects, students will perform a series of tests. These tests, along with other evidence or test results, will be used to solve a crime.",
-    link: "https://www.soinc.org/forensics-c"
+    link: "https://www.soinc.org/forensics-c",
   },
 ];
 
