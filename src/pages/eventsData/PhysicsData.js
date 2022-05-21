dd96091407ef3fe26c770333f5077fd6fd2229a3
Aaron <68828516+aaronbiscotti@users.noreply.github.com>
2022-05-21 10:14:15 -0400

    Update PhysicsData.js

--- src/pages/eventsData/PhysicsData.js
+++ src/pages/eventsData/PhysicsData.js
@@ -1,18 +1,21 @@
 const PhysicsData = [
   {
     eventName: "Codebusters",
+    path: "codebusters",
     description:
       "In Codebusters, up to 3 participants must decode encrypted messages, or they may be required to encode messages with certain advanced ciphers. Competitors are not allowed to bring any resources to this event, but can bring a 4 or 5 function calculator - not scientific or graphing calculators.",
     link: "https://www.soinc.org/codebusters-c",
   },
   {
     eventName: "It's About Time",
+    path: "its-about-time",
     description:
       "This event tests student's knowledge of time and their skill in building a timekeeping device. The test portion covers general topics in the physics and history of time, with everything in between. The second portion involves a timekeeping device to determine the amount of time, between 10 and 300 seconds (5 minutes), with a precision of 0.1 seconds. Please note, this is the best event in existence. It's About Time, It's About Power. We Stay Hungry. We Devour.",
     link: "https://www.soinc.org/its-about-time-c",
   },
   {
     eventName: "WiFi Lab",
+    path: "wifi-lab",
     description:
       "In WiFi lab, competitors are asked to build an antenna that transmits a signal at 2.4 GHz and complete a written test about electromagnetic waves. Teams may bring a three ring binder and two computational calculators, and are required to provide graphs and tables showing the relationship between power and distance for different configurations of the antenna.",
     link: "https://www.soinc.org/wifi-lab-c",
