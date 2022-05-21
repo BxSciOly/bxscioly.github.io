4a6e48e106c2a1ac5cb46ece0f3c04125dfa3032
Aaron <68828516+aaronbiscotti@users.noreply.github.com>
2022-05-21 10:06:28 -0400

    Update

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import BiologyData from "./eventsData/BiologyData";
 import styled from "styled-components";
 import BuildData from "./eventsData/BuildData";
 import ChemistryData from "./eventsData/ChemistryData";
 import PhysicsData from "./eventsData/PhysicsData";
 import EarthScienceData from "./eventsData/EarthScienceData";
+import Event from "./Event";
+import "./style/EventsStyling.css";
 
 export const Events = () => {
   return (
@@ -17,22 +20,38 @@ export const Events = () => {
 export const Biology = () => {
   return (
     <div className="events">
-      <Cover>BIOLOGY</Cover>
+      <Cover>BIOLOGY 🧬</Cover>
       <Link
         href="https://www.soinc.org/sites/default/files/2021-09/Science_Olympiad_Div_C_2022_Rules_Manual_Web_1.pdf"
         target="_blank"
         rel="noreferrer"
+        className="info"
       >
         Want more information? Click here for the event manual!
       </Link>
-      <CardDiv>
-        {BiologyData.map((event) => (
-          <BubbleCard href={event.link} target="_blank">
-            <EventTitle>{event.eventName}</EventTitle>
-            <EventDesc>{event.description}</EventDesc>
-          </BubbleCard>
-        ))}
-      </CardDiv>
+      <div className="wrap">
+        <div className="links">
+          <div className="bar" />
+          <h3 className="update" style={{ fontSize: "16px" }}>
+            Updated 2021
+          </h3>
+          <EventAnchor to="/biology">Introduction</EventAnchor>
+          <EventAnchor to="anatomy-and-physiology">
+            Anatomy and Physiology
+          </EventAnchor>
+          <EventAnchor to="cell-biology">Cell Biology</EventAnchor>
+          <EventAnchor to="disease-detectives">Disease Detectives</EventAnchor>
+          <EventAnchor to="ornithology">Ornithology</EventAnchor>
+          <EventAnchor to="green-generation">Green Generation</EventAnchor>
+        </div>
+        <div className="events-list">
+          <Event
+            dataSet={BiologyData}
+            subject="biology"
+            titlesubject="Biology"
+          />
+        </div>
+      </div>
     </div>
   );
 };
@@ -40,26 +59,35 @@ export const Biology = () => {
 export const EarthScience = () => {
   return (
     <div className="events">
-      <Cover>EARTH SCIENCE</Cover>
+      <Cover>EARTH SCIENCE 🌎</Cover>
       <Link
         href="https://www.soinc.org/sites/default/files/2021-09/Science_Olympiad_Div_C_2022_Rules_Manual_Web_1.pdf"
         target="_blank"
         rel="noreferrer"
+        className="info"
       >
         Want more information? Click here for the event manual!
       </Link>
-      <CardDiv>
-        {EarthScienceData.map((event) => (
-          <BubbleCard href={event.link} target="_blank">
-            <EventTitle>{event.eventName}</EventTitle>
-            <EventDesc>{event.description}</EventDesc>
-            <br />
-            <a href={event.links} target="_blank" rel="noopener noreferrer">
-              <EventLink>{event.links}</EventLink>
-            </a>
-          </BubbleCard>
-        ))}
-      </CardDiv>
+      <div className="wrap">
+        <div className="links">
+          <div className="bar" />
+          <h3 className="update" style={{ fontSize: "16px" }}>
+            Updated 2021
+          </h3>
+          <EventAnchor to="/earthscience">Introduction</EventAnchor>
+          <EventAnchor to="astronomy">Astronomy</EventAnchor>
+          <EventAnchor to="dynamic-planet">Dynamic Planet</EventAnchor>
+          <EventAnchor to="remote-sensing">Remote Sensing</EventAnchor>
+          <EventAnchor to="rocks-and-minerals">Rocks and Minerals</EventAnchor>
+        </div>
+        <div className="events-list">
+          <Event
+            dataSet={EarthScienceData}
+            subject="earthscience"
+            titlesubject="Earth Science"
+          />
+        </div>
+      </div>
     </div>
   );
 };
@@ -67,22 +95,43 @@ export const EarthScience = () => {
 export const Build = () => {
   return (
     <div className="events">
-      <Cover>BUILD</Cover>
+      <Cover>BUILD 🔧</Cover>
       <Link
         href="https://www.soinc.org/sites/default/files/2021-09/Science_Olympiad_Div_C_2022_Rules_Manual_Web_1.pdf"
         target="_blank"
         rel="noreferrer"
+        className="info"
       >
         Want more information? Click here for the event manual!
       </Link>
-      <CardDiv>
-        {BuildData.map((event) => (
-          <BubbleCard href={event.link} target="_blank">
-            <EventTitle>{event.eventName}</EventTitle>
-            <EventDesc>{event.description}</EventDesc>
-          </BubbleCard>
-        ))}
-      </CardDiv>
+      <div className="wrap">
+        <div className="links">
+          <div className="bar" />
+          <h3 className="update" style={{ fontSize: "16px" }}>
+            Updated 2021
+          </h3>
+          <EventAnchor to="/build">Introduction</EventAnchor>
+          <EventAnchor to="bridges">Bridges</EventAnchor>
+          <EventAnchor to="detector-building">Detector Building</EventAnchor>
+          <EventAnchor to="wright-stuff">Wright Stuff</EventAnchor>
+          <EventAnchor to="experimental-design">
+            Experimental Design
+          </EventAnchor>
+          <EventAnchor to="gravity-vehicle">Gravity Vehicle</EventAnchor>
+          <EventAnchor to="ping-pong-parachute">
+            Ping Pong Parachute
+          </EventAnchor>
+          <EventAnchor to="trajectory">Trajectory</EventAnchor>
+          <EventAnchor to="write-it-do-it">Write It Do It</EventAnchor>
+        </div>
+        <div className="events-list">
+          <Event
+            dataSet={BuildData}
+            subject="build"
+            titlesubject="Build and Engineering"
+          />
+        </div>
+      </div>
     </div>
   );
 };
@@ -90,22 +139,36 @@ export const Build = () => {
 export const Chemistry = () => {
   return (
     <div className="events">
-      <Cover>CHEMISTRY</Cover>
+      <Cover>CHEMISTRY 🧪</Cover>
       <Link
         href="https://www.soinc.org/sites/default/files/2021-09/Science_Olympiad_Div_C_2022_Rules_Manual_Web_1.pdf"
         target="_blank"
         rel="noreferrer"
+        className="info"
       >
         Want more information? Click here for the event manual!
       </Link>
-      <CardDiv>
-        {ChemistryData.map((event) => (
-          <BubbleCard href={event.link} target="_blank">
-            <EventTitle>{event.eventName}</EventTitle>
-            <EventDesc>{event.description}</EventDesc>
-          </BubbleCard>
-        ))}
-      </CardDiv>
+      <div className="wrap">
+        <div className="links">
+          <div className="bar" />
+          <h3 className="update" style={{ fontSize: "16px" }}>
+            Updated 2021
+          </h3>
+          <EventAnchor to="/chemistry">Introduction</EventAnchor>
+          <EventAnchor to="chemistry-lab">Chemistry Lab</EventAnchor>
+          <EventAnchor to="environmental-chemistry">
+            Environmental Chemistry
+          </EventAnchor>
+          <EventAnchor to="forensics">Forensics</EventAnchor>
+        </div>
+        <div className="events-list">
+          <Event
+            dataSet={ChemistryData}
+            subject="chemistry"
+            titlesubject="Chemistry"
+          />
+        </div>
+      </div>
     </div>
   );
 };
@@ -113,38 +176,49 @@ export const Chemistry = () => {
 export const Physics = () => {
   return (
     <div className="events">
-      <Cover>MATH & PHYSICS</Cover>
+      <Cover>MATH & PHYSICS 🍎🧮</Cover>
       <Link
         href="https://www.soinc.org/sites/default/files/2021-09/Science_Olympiad_Div_C_2022_Rules_Manual_Web_1.pdf"
         target="_blank"
         rel="noreferrer"
+        className="info"
       >
         Want more information? Click here for the event manual!
       </Link>
-      <CardDiv>
-        {PhysicsData.map((event) => (
-          <BubbleCard href={event.link} target="_blank">
-            <EventTitle>{event.eventName}</EventTitle>
-            <EventDesc>{event.description}</EventDesc>
-          </BubbleCard>
-        ))}
-      </CardDiv>
+      <div className="wrap">
+        <div className="links">
+          <div className="bar" />
+          <h3 className="update" style={{ fontSize: "16px" }}>
+            Updated 2021
+          </h3>
+          <EventAnchor to="/physics">Introduction</EventAnchor>
+          <EventAnchor to="codebusters">Codebusters</EventAnchor>
+          <EventAnchor to="its-about-time">It's About Time</EventAnchor>
+          <EventAnchor to="wifi-lab">WiFi Lab</EventAnchor>
+        </div>
+        <div className="events-list">
+          <Event
+            dataSet={PhysicsData}
+            subject="physics"
+            titlesubject="Math & Physics"
+          />
+        </div>
+      </div>
     </div>
   );
 };
 
 // styling
 
-const CardDiv = styled.div`
-  padding: 20px 5px;
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
+const EventAnchor = styled(NavLink)`
+  padding: 0;
+  margin: 0;
 
-  @media (max-width: 600px) {
-    padding: 20px 0;
+  &:hover {
+    color: var(--secondary-color);
   }
 `;
+const CardDiv = styled.div``;
 
 const Link = styled.a`
   font-size: 220%;
@@ -154,19 +228,6 @@ const Link = styled.a`
     text-align: center;
     font-size: 150%;
   }
-`;
-
-const EventTitle = styled.h6`
-  font-size: 30px;
-  text-align: center;
-  margin-bottom: 2rem;
-  padding: 1rem;
-`;
-
-const EventDesc = styled.h5`
-  font-size: 25px;
-  font-weight: 200;
-  padding: 1rem;
 `;
 
 const EventLink = styled.h5`
@@ -185,44 +246,9 @@ const EventLink = styled.h5`
   }
 `;
 
-const BubbleCard = styled.a`
-  background-color: var(--secondary-color);
-  border-radius: 30px;
-  border: 2.5px solid var(--secondary-color);
-  width: 600px;
-  padding: 4rem;
-  margin: 2rem;
-  display: flex;
-  flex-direction: column;
-  min-height: 490px;
-  cursor: default;
-  transition: 300ms ease;
-
-  &:hover {
-    border: 2.5px solid var(--primary-color);
-    cursor: pointer;
-  }
-
-  @media (max-width: 600px) {
-    width: 100%;
-    color: #fff;
-    padding: 1rem;
-    margin: 1rem;
-  }
-`;
-
 const Cover = styled.h1`
-  color: #fff;
-  text-shadow: 0 0 10px var(--primary-color);
-  height: 200px;
-  width: 100%;
-  border-bottom: 3px solid var(--primary-color);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 4rem;
-  font-size: 100px;
-  margin: 0 10rem 4rem 10rem;
+  font-size: 60px;
+  margin: 0;
 
   @media (max-width: 600px) {
     font-size: 35px;
