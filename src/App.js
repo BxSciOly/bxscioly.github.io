31dc126bfbaca0deb19fdb2d456deae09156bd5f
Aaron <68828516+aaronbiscotti@users.noreply.github.com>
2022-05-21 10:05:01 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
-import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import {
   Biology,
@@ -12,24 +11,55 @@ import {
 import Faqs from "./pages/Faqs";
 import Footer from "./components/Footer";
 import About from "./pages/About";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import BiologyData from "./pages/eventsData/BiologyData";
+import EarthScienceData from "./pages/eventsData/EarthScienceData";
+import PhysicsData from "./pages/eventsData/PhysicsData";
+import ChemistryData from "./pages/eventsData/ChemistryData";
+import BuildData from "./pages/eventsData/BuildData";
 
 function App() {
   return (
-      <HashRouter>
-        <Sidebar />
-        <Switch>
-          <Redirect exact from="/" to="/home" />
-          <Route path="/home" exact component={Home} />
-          <Route path="/about" exact component={About} />
-          <Route path="/biology" exact component={Biology} />
-          <Route path="/earthscience" exact component={EarthScience} />
-          <Route path="/build" exact component={Build} />
-          <Route path="/chemistry" exact component={Chemistry} />
-          <Route path="/physics" exact component={Physics} />
-          <Route path="/faqs" exact component={Faqs} />
-        </Switch>
-        <Footer />
-      </HashRouter>
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="*" element={<Navigate to="/home" replace />} />
+        <Route path="/home" exact element={<Home />} />
+        <Route path="/about" exact element={<About />} />
+        <Route path="/biology" exact element={<Biology />}>
+          {BiologyData.map((event) => (
+            <Route path={event.path} element={<></>} />
+          ))}
+        </Route>
+        <Route path="/earthscience" exact element={<EarthScience />}>
+          {EarthScienceData.map((event) => (
+            <Route path={event.path} element={<></>} />
+          ))}
+        </Route>
+        <Route path="/build" exact element={<Build />}>
+          {BuildData.map((event) => (
+            <Route path={event.path} element={<></>} />
+          ))}
+        </Route>
+        <Route path="/chemistry" exact element={<Chemistry />}>
+          {ChemistryData.map((event) => (
+            <Route path={event.path} element={<></>} />
+          ))}
+        </Route>
+        <Route path="/physics" exact element={<Physics />}>
+          {PhysicsData.map((event) => (
+            <Route path={event.path} element={<></>} />
+          ))}
+        </Route>
+        <Route path="/faqs" exact element={<Faqs />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
